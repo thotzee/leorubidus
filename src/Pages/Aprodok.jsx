02e726828cc99a3d262fa18caecd3681a60fbca0
Toth Zoltan <thotzee@gmail.com>
2022-09-07 22:24:28 +0200

    leorubidus

--- src/Pages/Aprodok.jsx
+++ src/Pages/Aprodok.jsx
@@ -6,7 +6,7 @@ export const Aprodok = () => {
   return (
     <div>
         <h2 className='cim'>Apródcsaták</h2>
-        <p className='szöveg'>Rendezvényeinken a család legfiatalabb tagjai is szerephez juthatnak, még pedig a csatatéren. Az apródok vs martalócok programunk során a gyermekek a gaz martalócok ellen vonulnak megfelelő vezetéssel. A fegyverük félelmetes, zokniba töltött szivacs, mellyel kikergethetik a martalócokat a hevenyészett erődítésből. A győztes csata után a résztvevő fiatalok részt vehetnek eg apródavatáson, mely során lovagrendünk apródjaivá válnak.</p>
+        <p className='szöveg'>Rendezvényeinken a család legfiatalabb tagjai is szerephez juthatnak, még pedig a csatatéren. Az apródok vs martalócok programunk során a gyermekek a gaz martalócok ellen vonulnak megfelelő vezetéssel. A fegyverük félelmetes, zokniba töltött szivacs, mellyel kikergethetik a martalócokat a hevenyészett erődítésből. A győztes csata után a résztvevő fiatalok részt vehetnek egy apródavatáson, mely során lovagrendünk apródjaivá válnak.</p>
         <img alt='aprodcsata' src={require('../img/apródcsata.jpg')} className='aprod' />
     </div>
   )
